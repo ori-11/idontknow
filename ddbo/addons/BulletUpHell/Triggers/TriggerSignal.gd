1e26e0476c2425e406da50656a712702a7c3bb61
dohuyakrutoy11002 <muradmehtiev162@gmail.com>
2024-08-15 10:54:44 -0400

    dont leave the addons there

--- ddbo/addons/BulletUpHell/Triggers/TriggerSignal.gd
+++
@@ -1,8 +0,0 @@
-@icon("res://addons/BulletUpHell/Sprites/NodeIcons11.png")
-extends RichTextEffect
-class_name TriggerSignal
-
-@export_placeholder("Signal name") var sig:String = ""
-
-func _init():
-	resource_name = "TrigSig"
